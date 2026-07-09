c1e5fd3948c751b8505294cbfec6e3a0b7422e9c
Clamserty <85110067+Clamserty@users.noreply.github.com>
2026-07-10 06:13:30 +0800

    Delete src/dao directory

--- src/dao/TransaksiDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import model.Transaksi;
-import java.util.List;
-
-public interface TransaksiDao {
-    void simpanTransaksi(Transaksi transaksi);
-    List<Transaksi> readAllWithTiketName();
-}
--- src/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package dao;
-
-import model.User;
-
-public interface UserDao {
-    User login(String username, String password);
-}
